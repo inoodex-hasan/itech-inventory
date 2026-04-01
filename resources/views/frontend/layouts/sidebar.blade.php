7a3714adfdd6f19114bc908dd25ec3b57be221ee
Inoodex Hasan <hasan@inoodex.com>
2026-04-01 16:21:18 +0600

    menu icons updated

--- resources/views/frontend/layouts/sidebar.blade.php
+++ resources/views/frontend/layouts/sidebar.blade.php
@@ -63,10 +63,10 @@
                                 class="fe fe-plus-circle"></i> <span> Add Category</span></a> --}}
                         <a href="{{ route('brands.index') }}"
                             class="{{ Route::currentRouteName() == 'brand.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Brand List</span></a>
+                                class="fe fe-box"></i> <span> Brand List</span></a>
                         <a href="{{ route('products.index') }}"
                             class="{{ Route::currentRouteName() == 'products.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Product List</span></a>
+                                class="fe fe-box"></i> <span> Product List</span></a>
                     </li>
                 @endcan
 
@@ -75,10 +75,10 @@
                     <li class="">
                         <a href="{{ route('sales.create') }}"
                             class="{{ Route::currentRouteName() == 'sales.create' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i> <span> Add Sales</span></a>
+                                class="fe fe-shopping-cart"></i> <span> Add Sales</span></a>
                         <a href="{{ route('sales.index') }}"
                             class="{{ Route::currentRouteName() == 'sales.index' ? 'active' : '' }}"><i
-                                class="fe fe-list"></i> <span>Sales List</span></a>
+                                class="fe fe-shopping-bag"></i> <span>Sales List</span></a>
                     </li>
                 @endcan
 
@@ -96,7 +96,7 @@
                                 class="fe fe-check-square"></i> <span>Completed Service</span></a> --}}
                         <a href="{{ route('service.index', ['type' => 'service']) }}"
                             class="{{ Route::currentRouteName() == 'service.index' ? 'active' : '' }}"><i
-                                class="fe fe-server"></i> <span>Service List</span></a>
+                                class="fe fe-layers"></i> <span>Service List</span></a>
                     </li>
                 @endcan
 
@@ -105,10 +105,10 @@
                     <li class="">
                         <a href="{{ route('customers.create') }}"
                             class="{{ Route::currentRouteName() == 'customers.create' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i> <span> Add Customer</span></a>
+                                class="fe fe-user-plus"></i> <span> Add Customer</span></a>
                         <a href="{{ route('customers.index') }}"
                             class="{{ Route::currentRouteName() == 'customers.index' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i> <span>Customers List</span></a>
+                                class="fe fe-users"></i> <span>Customers List</span></a>
                     </li>
                 @endcan
 
@@ -117,10 +117,10 @@
                     <li class="">
                         <a href="{{ route('vendors.create') }}"
                             class="{{ Route::currentRouteName() == 'vendors.create' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i> <span> Add Vendor</span></a>
+                                class="fe fe-user-plus"></i> <span> Add Vendor</span></a>
                         <a href="{{ route('vendors.index') }}"
                             class="{{ Route::currentRouteName() == 'vendors.index' ? 'active' : '' }}"><i
-                                class="fe fe-list"></i> <span>Vendor List</span></a>
+                                class="fe fe-users"></i> <span>Vendor List</span></a>
                     </li>
                 @endcan
 
@@ -129,7 +129,7 @@
                     <li class="">
                         <a href="{{ route('purchase.index') }}"
                             class="{{ Route::currentRouteName() == 'purchase.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Purchase List</span></a>
+                                class="fe fe-shopping-cart"></i> <span> Purchase List</span></a>
                     </li>
                 @endcan
 
@@ -138,7 +138,7 @@
                     <li class="">
                         <a href="{{ route('company-details.index') }}"
                             class="{{ Route::currentRouteName() == 'company-details.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Company Details</span></a>
+                                class="fe fe-layers"></i> <span> Company Details</span></a>
                     </li>
                 @endcan
 
@@ -146,7 +146,7 @@
                     <li class="menu-title"><span>Payment Management</span></li>
                     <li>
                         <a href="{{ route('due-payments.index') }}"
-                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-plus-circle"></i>
+                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-dollar-sign"></i>
                             <span>Due Payment</span></a>
                     </li>
                     {{-- <li>
@@ -156,17 +156,17 @@
                     </li> --}}
                     <li>
                         <a href="{{ route('bills.index') }}"
-                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-plus-circle"></i>
+                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-file-text"></i>
                             <span>Bill Generate</span></a>
                     </li>
                     <li>
                         <a href="{{ route('challans.index') }}"
-                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-plus-circle"></i>
+                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-truck"></i>
                             <span>Challan Generate</span></a>
                     </li>
                     <li>
                         <a href="{{ route('quotations.index') }}"
-                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-plus-circle"></i>
+                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-clipboard"></i>
                             <span>Quotation Generate</span></a>
                     </li>
                 @endcan
@@ -176,7 +176,7 @@
                     <li class="">
                         <a href="{{ route('inventory.index') }}"
                             class="{{ Route::currentRouteName() == 'inventory.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Inventory List</span></a>
+                                class="fe fe-box"></i> <span> Inventory List</span></a>
                     </li>
                 @endcan
 
@@ -185,7 +185,7 @@
                     <li class="">
                         <a href="{{ route('clients.index') }}"
                             class="{{ Route::currentRouteName() == 'clients.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Clients List</span></a>
+                                class="fe fe-users"></i> <span> Clients List</span></a>
                     </li>
                 @endcan
 
@@ -194,10 +194,10 @@
                     <li class="">
                         <a href="{{ route('cost-categories.index') }}"
                             class="{{ Route::currentRouteName() == 'cost-categories.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Cost-Category List</span></a>
+                                class="fe fe-tag"></i> <span> Cost-Category List</span></a>
                         <a href="{{ route('cost-categories.create') }}"
                             class="{{ Route::currentRouteName() == 'cost-categories.create' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span>Add Cost-Category</span></a>
+                                class="fe fe-plus-circle"></i> <span>Add Cost-Category</span></a>
                     </li>
                 @endcan
 
@@ -209,7 +209,7 @@
                                 class="fe fe-list"></i> <span>TA/DA List</span></a>
                         <a href="{{ route('employee.tada.create') }}"
                             class="{{ Route::currentRouteName() == 'employee.tada.create' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span>Submit TA/DA</span></a>
+                                class="fe fe-upload"></i> <span>Submit TA/DA</span></a>
                     </li>
                 @endcan
 
@@ -218,14 +218,14 @@
                     <li class="">
                         <a href="{{ route('projects.index') }}"
                             class="{{ Route::currentRouteName() == 'projects.index' ? 'active' : '' }}"><i
-                                class="fe fe-list"></i> <span>Projects List</span></a>
+                                class="fe fe-briefcase"></i> <span>Projects List</span></a>
                         <a href="{{ route('project-items.index') }}"
                             class="{{ Route::currentRouteName() == 'project-items.index' ? 'active' : '' }}">
-                            <i class="fe fe-list"></i> <span>Add Projects Items</span>
+                            <i class="fe fe-plus-circle"></i> <span>Add Projects Items</span>
                         </a>
                         <a href="{{ route('project-costs.index') }}"
                             class="{{ Route::currentRouteName() == 'project-costs.index' ? 'active' : '' }}">
-                            <i class="fe fe-list"></i> <span>Projects Cost List</span>
+                            <i class="fe fe-dollar-sign"></i> <span>Projects Cost List</span>
                         </a>
                         {{-- <a href="{{ route('employee.tada.create') }}"
                             class="{{ Route::currentRouteName() == 'employee.tada.create' ? 'active' : '' }}"><i
@@ -238,7 +238,7 @@
                     <li>
                         <a href="{{ route('expense-categories.index') }}"
                             class="{{ Route::currentRouteName() == 'expense-categories.index' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i> <span>Expense Category</span></a>
+                                class="fe fe-tag"></i> <span>Expense Category</span></a>
                         <a href="{{ route('dailyExpenses.index') }}"
                             class="{{ Route::currentRouteName() == 'dailyExpenses.index' ? 'active' : '' }}"><i
                                 class="fe fe-list"></i> <span>Expense List</span></a>
@@ -247,17 +247,16 @@
                                 class="fe fe-plus-circle"></i> <span>Add Employee</span></a> --}}
                         <a href="{{ route('employees.index') }}"
                             class="{{ Route::currentRouteName() == 'employees.index' ? 'active' : '' }}"><i
-                                class="fe fe-list"></i> <span>Employee List</span></a>
+                                class="fe fe-users"></i> <span>Employee List</span></a>
                         <a href="{{ route('ta-da.index') }}"
                             class="{{ Route::currentRouteName() == 'ta-da.index' ? 'active' : '' }}"><i
                                 class="fe fe-list"></i> <span>TA/DA List</span></a>
                         <a href="{{ route('salary.index') }}"
                             class="{{ Route::currentRouteName() == 'salary.index' ? 'active' : '' }}"><i
-                                class="fe fe-list"></i> <span>Salary Manage</span></a>
+                                class="fe fe-dollar-sign"></i> <span>Salary Manage</span></a>
 
                         <a href="{{ route('bank-details.index') }}"
-                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i
-                                class="fe fe-plus-circle"></i>
+                            class="{{ Route::currentRouteName() == '' ? 'active' : '' }}"><i class="fe fe-layers"></i>
                             <span>Bank Details</span></a>
 
                     </li>
@@ -268,17 +267,17 @@
                     <li class="">
                         <a href="{{ route('purchase.report') }}"
                             class="{{ Route::currentRouteName() == 'purchase.report' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Purchase Report</span></a>
+                                class="fe fe-file-text"></i> <span> Purchase Report</span></a>
                     </li>
                     <li class="">
                         <a href="{{ route('sales.report') }}"
                             class="{{ Route::currentRouteName() == 'sales.report' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Sales Report</span></a>
+                                class="fe fe-file-text"></i> <span> Sales Report</span></a>
                     </li>
                     <li class="">
                         <a href="{{ route('revenues.index') }}"
                             class="{{ Route::currentRouteName() == 'revenues.index' ? 'active' : '' }}"><i
-                                class="fe fe-package"></i> <span> Revenue Report</span></a>
+                                class="fe fe-bar-chart-2"></i> <span> Revenue Report</span></a>
                         </a>
                     </li>
                 @endcan
