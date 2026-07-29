6e8fc0d1556234913c26674cd755811865d65e66
Inoodex Hasan <hasan@inoodex.com>
2026-07-29 17:25:11 +0600

    fix: make header structure 100% unified and identical across all pages and error pages

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -43,190 +43,58 @@
     </a>
     <!-- /Mobile Menu Toggle -->
 
-    @auth
-        <!-- Search -->
-        <div class="top-nav-search d-none">
-            <form>
-                <input type="text" class="form-control" placeholder="Search here">
-                <button class="btn" type="submit"><img src="{{ asset('assets') }}/img/icons/search.svg"
-                        alt="img"></button>
-            </form>
-        </div>
-        <!-- /Search -->
+    <!-- Search -->
+    <div class="top-nav-search d-none">
+        <form>
+            <input type="text" class="form-control" placeholder="Search here">
+            <button class="btn" type="submit"><img src="{{ asset('assets') }}/img/icons/search.svg"
+                    alt="img"></button>
+        </form>
+    </div>
+    <!-- /Search -->
 
-        <!-- Header Menu -->
-        <ul class="nav nav-tabs user-menu">
-            <!-- Flag -->
-            <li class="nav-item dropdown has-arrow flag-nav d-none">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">
-                    <img src="{{ asset('assets') }}/img/flags/us1.png" alt="flag"><span>English</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <img src="{{ asset('assets') }}/img/flags/us.png" alt="flag"><span>English</span>
-                    </a>
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <img src="{{ asset('assets') }}/img/flags/fr.png" alt="flag"><span>French</span>
-                    </a>
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <img src="{{ asset('assets') }}/img/flags/es.png" alt="flag"><span>Spanish</span>
-                    </a>
-                    <a href="javascript:void(0);" class="dropdown-item">
-                        <img src="{{ asset('assets') }}/img/flags/de.png" alt="flag"><span>German</span>
-                    </a>
-                </div>
-            </li>
-            <!-- /Flag -->
-            <li class="nav-item dropdown  flag-nav dropdown-heads">
-                <a class="nav-link d-none" data-bs-toggle="dropdown" href="#" role="button">
-                    <i class="fe fe-bell"></i> <span class="badge rounded-pill"></span>
-                </a>
-                <div class="dropdown-menu notifications d-none">
-                    <div class="topnav-dropdown-header">
-                        <div class="notification-title">Notifications <a href="notifications.html">View all</a></div>
-                        <a href="javascript:void(0)" class="clear-noti d-flex align-items-center">Mark all as read <i
-                                class="fe fe-check-circle"></i></a>
-                    </div>
-                    <div class="noti-content">
-                        <ul class="notification-list">
-                            <li class="notification-message">
-                                <a href="profile.html">
-                                    <div class="d-flex">
-                                        <span class="avatar avatar-md active">
-                                            <img class="avatar-img rounded-circle" alt="avatar-img"
-                                                src="{{ asset('assets') }}/img/profiles/avatar-02.jpg">
-                                        </span>
-                                        <div class="media-body">
-                                            <p class="noti-details"><span class="noti-title">Lex Murphy</span> requested
-                                                access to <span class="noti-title">UNIX directory tree hierarchy</span></p>
-                                            <div class="notification-btn">
-                                                <span class="btn btn-primary">Accept</span>
-                                                <span class="btn btn-outline-primary">Reject</span>
-                                            </div>
-                                            <p class="noti-time"><span class="notification-time">Today at 9:42 AM</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="notification-message">
-                                <a href="profile.html">
-                                    <div class="d-flex">
-                                        <span class="avatar avatar-md active">
-                                            <img class="avatar-img rounded-circle" alt="avatar-img"
-                                                src="{{ asset('assets') }}/img/profiles/avatar-10.jpg">
-                                        </span>
-                                        <div class="media-body">
-                                            <p class="noti-details"><span class="noti-title">Ray Arnold</span> left 6
-                                                comments <span class="noti-title">on Isla Nublar SOC2 compliance
-                                                    report</span></p>
-                                            <p class="noti-time"><span class="notification-time">Yesterday at 11:42
-                                                    PM</span></p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="notification-message">
-                                <a href="profile.html">
-                                    <div class="d-flex">
-                                        <span class="avatar avatar-md">
-                                            <img class="avatar-img rounded-circle" alt="avatar-img"
-                                                src="{{ asset('assets') }}/img/profiles/avatar-13.jpg">
-                                        </span>
-                                        <div class="media-body">
-                                            <p class="noti-details"><span class="noti-title">Dennis Nedry</span> commented
-                                                on <span class="noti-title"> Isla Nublar SOC2 compliance report</span></p>
-                                            <blockquote>
-                                                “Oh, I finished de-bugging the phones, but the system's compiling for
-                                                eighteen minutes, or twenty. So, some minor systems may go on and off for a
-                                                while.”
-                                            </blockquote>
-                                            <p class="noti-time"><span class="notification-time">Yesterday at 5:42
-                                                    PM</span></p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="notification-message">
-                                <a href="profile.html">
-                                    <div class="d-flex">
-                                        <span class="avatar avatar-md">
-                                            <img class="avatar-img rounded-circle" alt="avatar-img"
-                                                src="{{ asset('assets') }}/img/profiles/avatar-05.jpg">
-                                        </span>
-                                        <div class="media-body">
-                                            <p class="noti-details"><span class="noti-title">John Hammond</span> created
-                                                <span class="noti-title">Isla Nublar SOC2 compliance report</span>
-                                            </p>
-                                            <p class="noti-time"><span class="notification-time">Last Wednesday at 11:15
-                                                    AM</span></p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="topnav-dropdown-footer">
-                        <a href="#">Clear All</a>
-                    </div>
-                </div>
-            </li>
-            <li class="nav-item  has-arrow dropdown-heads ">
-                <a href="javascript:void(0);" class="win-maximize">
-                    <i class="fe fe-maximize"></i>
-                </a>
-            </li>
-            <!-- User Menu -->
-            <li class="nav-item dropdown">
-                <a href="javascript:void(0)" class="user-link  nav-link" data-bs-toggle="dropdown">
-                    <span class="user-img">
-                        <img src="{{ asset('frontend/users/' . auth()->user()->images) }}"
-                            onerror="this.src='{{ asset('assets/img/favicon.png') }}'" alt=""
-                            class="profilesidebar">
-                        <span class="animate-circle"></span>
-                    </span>
-                    <span class="user-content">
-                        <!-- <span class="user-details">Admin</span> -->
-                        <span class="user-name">{{ auth()->user()->name }}</span>
-                    </span>
-                </a>
-                <div class="dropdown-menu menu-drop-user">
-                    <div class="profilemenu">
-                        <div class="subscription-menu">
-                            <ul>
-                                <li class="d-none">
-                                    <a class="dropdown-item" href="profile.html">Profile</a>
-                                </li>
-                                <li class="d-none">
-                                    <a class="dropdown-item" href="settings.html">Settings</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="subscription-logout">
-                            <ul>
-                                <li class="pb-0">
+    <!-- Header Menu -->
+    <ul class="nav nav-tabs user-menu">
+        <li class="nav-item has-arrow dropdown-heads">
+            <a href="javascript:void(0);" class="win-maximize">
+                <i class="fe fe-maximize"></i>
+            </a>
+        </li>
+        <!-- User Menu -->
+        <li class="nav-item dropdown">
+            <a href="javascript:void(0)" class="user-link nav-link" data-bs-toggle="dropdown">
+                <span class="user-img">
+                    <img src="{{ auth()->check() && auth()->user()->images ? asset('frontend/users/' . auth()->user()->images) : asset('assets/img/favicon.png') }}"
+                        onerror="this.src='{{ asset('assets/img/favicon.png') }}'" alt=""
+                        class="profilesidebar">
+                    <span class="animate-circle"></span>
+                </span>
+                <span class="user-content">
+                    <span class="user-name">{{ auth()->check() ? auth()->user()->name : 'Guest' }}</span>
+                </span>
+            </a>
+            <div class="dropdown-menu menu-drop-user">
+                <div class="profilemenu">
+                    <div class="subscription-logout">
+                        <ul>
+                            <li class="pb-0">
+                                @auth
                                     <a onclick="document.getElementById('logoutForm').submit()" class="dropdown-item"
                                         href="javascript:void(0)">Log Out</a>
                                     <form id="logoutForm" action="{{ route('logout') }}" method="post">
                                         @csrf
                                     </form>
-                                </li>
-                            </ul>
-                        </div>
+                                @else
+                                    <a class="dropdown-item" href="{{ route('login') }}">Log In</a>
+                                @endauth
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </li>
-            <!-- /User Menu -->
-
-        </ul>
-
-        <!-- /Header Menu -->
-    @else
-        <ul class="nav nav-tabs user-menu">
-            <li class="nav-item">
-                <a href="{{ route('login') }}" class="btn btn-primary btn-sm my-auto px-3 py-2 fw-semibold">Log In</a>
-            </li>
-        </ul>
-    @endauth
+            </div>
+        </li>
+        <!-- /User Menu -->
+    </ul>
+    <!-- /Header Menu -->
 </div>
 <!-- /Header -->
